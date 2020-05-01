548a6f0bdbdaab4d2be159ebb2c6908d89ab9632
Isaac Worsley <itw21@uclive.ac.nz>
2020-05-02 11:34:27 +1200

    Added initial sign up screen.

--- app/src/main/java/nz/ac/uclive/itw21/project2/MainActivity.kt
+++ app/src/main/java/nz/ac/uclive/itw21/project2/MainActivity.kt
@@ -1,12 +1,15 @@
 package nz.ac.uclive.itw21.project2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import nz.ac.uclive.itw21.project2.ui.CreateProfileActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(baseContext, CreateProfileActivity::class.java));
     }
 }
---
+++ app/src/main/java/nz/ac/uclive/itw21/project2/ui/CreateProfileActivity.kt
@@ -0,0 +1,13 @@
+package nz.ac.uclive.itw21.project2.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nz.ac.uclive.itw21.project2.R
+
+class CreateProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile)
+    }
+}
